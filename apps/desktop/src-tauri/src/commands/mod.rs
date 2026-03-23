684bd4b7822be182a8881c98262c7dc99d02d5c3
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-03-23 11:20:58 +0800

    refactor(desktop): trim unused command reexports

--- apps/desktop/src-tauri/src/commands/mod.rs
+++ apps/desktop/src-tauri/src/commands/mod.rs
@@ -5,16 +5,3 @@ pub mod reminder_commands;
 pub mod sync_commands;
 pub mod tag_commands;
 pub mod task_commands;
-
-pub use bench_commands::seed_benchmark_data;
-pub use data_commands::{export_csv, export_data, import_data};
-pub use list_commands::{create_list, delete_list, get_lists, update_list};
-pub use sync_commands::{
-    dismiss_sync_conflict, get_sync_health, get_sync_settings, list_sync_conflicts,
-    resolve_sync_conflict, save_sync_settings, sync_now,
-};
-pub use task_commands::{
-    complete_recurring_task, create_task, delete_task, get_overdue_tasks, get_task,
-    get_tasks_by_list, get_tasks_due_today, get_tasks_in_range, move_task, preview_recurrence,
-    search_tasks, update_task,
-};
