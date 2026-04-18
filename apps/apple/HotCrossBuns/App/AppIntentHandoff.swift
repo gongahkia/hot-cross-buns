01d201fcdd411b44a062d79b3b2de0cd3ba86259
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-04-18 13:27:41 +0800

    feat(apple): add app shortcut handoffs

---
+++ apps/apple/HotCrossBuns/App/AppIntentHandoff.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+enum AppIntentRoute: String, Sendable {
+    case addTask
+    case addEvent
+    case today
+}
+
+enum AppIntentHandoff {
+    private static let pendingRouteKey = "appIntent.pendingRoute"
+
+    static func save(_ route: AppIntentRoute) {
+        UserDefaults.standard.set(route.rawValue, forKey: pendingRouteKey)
+    }
+
+    static func consumePendingRoute() -> AppIntentRoute? {
+        guard let rawValue = UserDefaults.standard.string(forKey: pendingRouteKey) else {
+            return nil
+        }
+
+        UserDefaults.standard.removeObject(forKey: pendingRouteKey)
+        return AppIntentRoute(rawValue: rawValue)
+    }
+}
