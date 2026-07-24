214c84df9ee5f441cbfec8be61eec0f226f29da5
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-07-24 23:49:35 +0800

    feat(native): apply durable sqlite pragmas

--- native/tests/SqliteConnectionTest.cpp
+++ native/tests/SqliteConnectionTest.cpp
@@ -15,11 +15,87 @@ class SqliteConnectionTest final : public QObject {
   Q_OBJECT
 
 private slots:
+  void appliesProductionPragmas();
   void opensReadWriteConnectionAndCreatesDatabase();
   void opensExistingDatabaseReadOnly();
   void reportsDatabaseErrorForMissingReadOnlyFile();
   void rejectsSymbolicLink();
 };
+
+namespace {
+
+void verifyProductionPragmas(sqlite3* handle) {
+  sqlite3_stmt* statement = nullptr;
+  QCOMPARE(sqlite3_prepare_v2(handle, "PRAGMA foreign_keys", -1, &statement, nullptr), SQLITE_OK);
+  QCOMPARE(sqlite3_step(statement), SQLITE_ROW);
+  QCOMPARE(sqlite3_column_int64(statement, 0), 1);
+  QCOMPARE(sqlite3_finalize(statement), SQLITE_OK);
+
+  QCOMPARE(sqlite3_prepare_v2(handle, "PRAGMA journal_mode", -1, &statement, nullptr), SQLITE_OK);
+  QCOMPARE(sqlite3_step(statement), SQLITE_ROW);
+  QCOMPARE(QString::fromUtf8(reinterpret_cast<const char*>(sqlite3_column_text(statement, 0))),
+           QStringLiteral("wal"));
+  QCOMPARE(sqlite3_finalize(statement), SQLITE_OK);
+
+  QCOMPARE(sqlite3_prepare_v2(handle, "PRAGMA synchronous", -1, &statement, nullptr), SQLITE_OK);
+  QCOMPARE(sqlite3_step(statement), SQLITE_ROW);
+  QCOMPARE(sqlite3_column_int64(statement, 0), 2);
+  QCOMPARE(sqlite3_finalize(statement), SQLITE_OK);
+
+  QCOMPARE(sqlite3_prepare_v2(handle, "PRAGMA temp_store", -1, &statement, nullptr), SQLITE_OK);
+  QCOMPARE(sqlite3_step(statement), SQLITE_ROW);
+  QCOMPARE(sqlite3_column_int64(statement, 0), 2);
+  QCOMPARE(sqlite3_finalize(statement), SQLITE_OK);
+
+  QCOMPARE(sqlite3_prepare_v2(handle, "PRAGMA cache_size", -1, &statement, nullptr), SQLITE_OK);
+  QCOMPARE(sqlite3_step(statement), SQLITE_ROW);
+  QCOMPARE(sqlite3_column_int64(statement, 0), -65'536);
+  QCOMPARE(sqlite3_finalize(statement), SQLITE_OK);
+
+  QCOMPARE(sqlite3_prepare_v2(handle, "PRAGMA mmap_size", -1, &statement, nullptr), SQLITE_OK);
+  QCOMPARE(sqlite3_step(statement), SQLITE_ROW);
+  QCOMPARE(sqlite3_column_int64(statement, 0), 268'435'456);
+  QCOMPARE(sqlite3_finalize(statement), SQLITE_OK);
+
+  QCOMPARE(sqlite3_prepare_v2(handle, "PRAGMA busy_timeout", -1, &statement, nullptr), SQLITE_OK);
+  QCOMPARE(sqlite3_step(statement), SQLITE_ROW);
+  QCOMPARE(sqlite3_column_int64(statement, 0), 30'000);
+  QCOMPARE(sqlite3_finalize(statement), SQLITE_OK);
+}
+
+} // namespace
+
+void SqliteConnectionTest::appliesProductionPragmas() {
+  QTemporaryDir temporaryDirectory;
+  QVERIFY(temporaryDirectory.isValid());
+  const std::optional<hcb::FilePath> databasePath =
+      hcb::FilePath::fromAbsolute(QDir(QFileInfo(temporaryDirectory.path()).canonicalFilePath())
+                                      .filePath(QStringLiteral("hot-cross-buns.sqlite")));
+  QVERIFY(databasePath.has_value());
+  if (!databasePath.has_value()) {
+    return;
+  }
+
+  {
+    hcb::SqliteConnectionResult writable =
+        hcb::SqliteConnectionFactory::open(*databasePath, hcb::SqliteOpenMode::ReadWriteCreate);
+    QVERIFY(std::holds_alternative<hcb::SqliteConnection>(writable));
+    if (!std::holds_alternative<hcb::SqliteConnection>(writable)) {
+      return;
+    }
+    hcb::SqliteConnection writeConnection = std::move(std::get<hcb::SqliteConnection>(writable));
+    verifyProductionPragmas(writeConnection.nativeHandle());
+  }
+
+  hcb::SqliteConnectionResult readOnly =
+      hcb::SqliteConnectionFactory::open(*databasePath, hcb::SqliteOpenMode::ReadOnly);
+  QVERIFY(std::holds_alternative<hcb::SqliteConnection>(readOnly));
+  if (!std::holds_alternative<hcb::SqliteConnection>(readOnly)) {
+    return;
+  }
+  hcb::SqliteConnection readConnection = std::move(std::get<hcb::SqliteConnection>(readOnly));
+  verifyProductionPragmas(readConnection.nativeHandle());
+}
 
 void SqliteConnectionTest::opensReadWriteConnectionAndCreatesDatabase() {
   QTemporaryDir temporaryDirectory;
